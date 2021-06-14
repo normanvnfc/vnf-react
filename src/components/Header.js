007d08cdb01f38f989d9de0da60faf3051628b58
normanvnfc <norman@venuerific.com>
2021-06-14 16:08:26 +0700

    Adding asset and recomposing header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,24 @@
+import logo from '../assets/img/nav-logo.png'
+
 const Header = () => {
   return (
-    <h1>Test</h1>
+    <header className="header">
+      <nav className="nav-main navbar navbar-expand-lg bg-white shadow-sm">
+        <div className="container">
+          <a className="logo" href="https://www.venuerific.com/sg">
+            <h1>test</h1>
+            <figure className="mb-0 logo">
+              <img src={logo} alt="" />
+            </figure>
+          </a>
+        </div>
+        <div className="country-selector navbar-expand">
+          <ul className="navbar-nav">
+            
+          </ul>
+        </div>
+      </nav>
+    </header>
   )
 }
 
